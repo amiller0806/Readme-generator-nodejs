adef03f2a77381ff826392dcfa3fa37b5d8676d0
amiller0806 <ariel.miller555@gmail.com>
2022-11-25 11:45:13 -0500

    entered questions, initalized app, tested app, ensured success

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -15,8 +15,8 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  console.log(data);
   return `# ${data.title}
-
 `;
 }
 
